9c39a29af6adc21e9c7d5f68d420613869524265
Fabian Drechsler <ets.stiffler@gmail.com>
2018-08-04 18:09:52 +0200

    update

--- bin/webserver/entrypoint.sh
+++ bin/webserver/entrypoint.sh
@@ -1,7 +1,9 @@
+#!/bin/sh
+
 echo "/bin/false" > nano /etc/shells
 
 
-adduser joomla1 --ingroup www-data --shell /bin/false --home /var/www --no-create-home --gecos "" --disable-password
+adduser joomla1 --ingroup www-data --shell /bin/false --home /var/www --no-create-home --gecos "" --disabled-password
 echo joomla1:joomla1 | chpasswd
 
 
