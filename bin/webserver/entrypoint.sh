48dffc59162e423487adf6cef809531a08d971f3
Fabian Drechsler <ets.stiffler@gmail.com>
2018-08-04 17:00:49 +0200

    update

---
+++ bin/webserver/entrypoint.sh
@@ -0,0 +1,6 @@
+/etc/init.d/vsftpd restart
+echo "/bin/false" > nano /etc/shells
+
+
+adduser joomla1 --ingroup www-data --shell /bin/false --home /var/www --no-create-home --gecos "" --disable-password
+echo joomla1:joomla1 | chpasswd
