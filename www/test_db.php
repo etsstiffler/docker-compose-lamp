050111bddaab57c55ea6455ace11ef5a1f4bcff3
Fabian Drechsler <ets.stiffler@gmail.com>
2018-08-06 18:47:46 +0200

    fixed ftp connection

--- www/test_db.php
+++ www/test_db.php
@@ -1,5 +1,5 @@
 <?php
-$link = mysqli_connect("mysql", "kursd", "kursd", "kursd");
+$link = mysqli_connect("mysql", "root", "tiger", "mysql");
 
 if (!$link) {
     echo "Error: Unable to connect to MySQL." . PHP_EOL;
