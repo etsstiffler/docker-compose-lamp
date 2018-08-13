ba02da7c8463dfee1ebd4283c07923f5597adde7
Fabian Drechsler <ets.stiffler@gmail.com>
2018-08-13 10:39:40 +0200

    fixes

---
+++ bin/vsftpd/conf/provision/entrypoint.d/20-vsftp.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+if ! id -u "$FTP_USER" > /dev/null 2>&1; then
+    # Add group
+    groupadd -g "$FTP_GID" "$FTP_USER"
+
+    # Add user
+    useradd -u "$FTP_UID" --create-home --shell /bin/bash --no-user-group "$FTP_USER"
+
+    # Assign user to group
+    usermod -g "$FTP_USER" "$FTP_USER"
+fi
+
+# Set passwords to "dev"
+echo "$FTP_USER":"$FTP_PASSWORD" | chpasswd
+
+useradd joomla1 --ingroup dev --shell /bin/false --home /storage/ftp/joomla1  --gecos "" --disabled-password
+echo "joomla1":"joomla1" | chpasswd
